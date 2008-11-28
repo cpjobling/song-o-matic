88c5726ecf0b1f3fb812049d47bd4ecac8fa1096
Dr Chris P. Jobling <C.P.Jobling@Swansea.ac.uk>
2008-11-28 23:56:39 +0000

    Correct up to step 5

--- db/migrate/20081128191928_populate_db.rb
+++ db/migrate/20081128191928_populate_db.rb
@@ -1,40 +1,37 @@
 class PopulateDb < ActiveRecord::Migration
   def self.up
   	beatles = "The Beatles"
-  	LandM = "John Lennon and Paul McCartney"
+  	landm = "John Lennon and Paul McCartney"
   	
-  	help = Songs.create(:title => "Help!", 
-  		:composer => LandM, 
+  	help = Song.create(:title => "Help!", 
+  		:composer => landm, 
   		:artist_or_group => beatles)
   	help.save!
   	
-  	please = Songs.create(:title => "Please, Please Me",
-  		:composer => LandM,
-  		:artist_or_group = beatles)
+  	please = Song.create(:title => "Please, Please Me",
+  		:composer => landm,
+  		:artist_or_group => beatles)
+  	please.save!
   	
-  	space = Songs.create(:title => "Space Oddity", 
+  	space = Song.create(:title => "Space Oddity", 
   		:composer => "David Bowie", 
   		:artist_or_group => "David Bowie and the Spiders from Mars")
   	space.save!
   	
-  	subway = Songs.create(:title => "Don't Sleep in the Subway Darling", 
+  	subway = Song.create(:title => "Don't Sleep in the Subway Darling", 
   		:composer => "Jackie Trent and Tony Hatch", 
   		:artist_or_group => "Petula Clark")
   	subway.save!
   	
-  	wish = Songs.create(:title => "Wish You Where Here", 
+  	wish = Song.create(:title => "Wish You Where Here", 
   		:composer => "Roger Waters and David Gilmour", 
   		:artist_or_group => "Pink Floyd")
   	wish.save!
   	
-    subway = Songs.create(:title => "Don't Sleep in the Subway Darling", 
-  		:composer => "Jackie Trent and Tony Hatch", 
-  		:artist_or_group => "Petula Clark")
-  	subway.save!
-  	
-  	magic = Songs.create(:title => "It's a Kind of Magic",
+  	magic = Song.create(:title => "It's a Kind of Magic",
   		:composer => "Freddy Mercury",
-        :artist_or_group => "Queen";
+        :artist_or_group => "Queen")
+    magic.save!
   	
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081128163400) do
+ActiveRecord::Schema.define(:version => 20081128191928) do
 
   create_table "songs", :force => true do |t|
     t.string   "title"
